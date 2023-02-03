b1492a218b103c7a8c872f8934c9a37ff52b9cb0
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2023-02-03 01:00:28 -0500

    add  modal, pack and unpack (has issues)

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,12 +13,12 @@ export const Menu = () => {
             <p className="lead">Please scan or enter Carton / Order number to proceed</p>
             <form>
 
-                <div className="input-group w-25">
+                <div className="input-group">
                     <span className="input-group-text">Carton #</span>
                     <input type="text" className="form-control" id="cartonNumber" placeholder="Carton number" />
                 </div>
                 <br />
-                <div className="input-group w-25">
+                <div className="input-group">
                     <span className="input-group-text">Order #</span>
                     <input type="text" className="form-control" id="orderNumber" placeholder="Order number" />
                 </div>
--- src/components/Packing.js
+++ src/components/Packing.js
@@ -1,19 +1,96 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import Layout from './Layout';
-import { Action } from "./Action";
+import JSONpickData from "./data.json";
+import "../styles/table.css"
 
 export const Packing = () => {
 
+    const [selectedOrderLine, setSelectedOrderLine] = useState();
+    const [packData, setPackData] = useState([]);
+    const [pickData, setPickData] = useState(JSONpickData);
+
+    const [showModal, setShowModal] = useState(false);
+
     const header = ["Order Line", "SKU", "Picked Qty", "Remaining Qty", "Description", "UPC"];
 
-    const pickData = [
-        { "orderLine": 1, "sku": "101-0909", "pickedQty": 10, "remainingQty": 5, "description": "iPhone 12", "upc": "123456789012", },
-        { "orderLine": 2, "sku": "101-0910", "pickedQty": 100, "remainingQty": 50, "description": "iPhone 12 Pro", "upc": "123456789013", },
-    ];
+    const toggleRow = (orderLine) => {
+        if (selectedOrderLine === orderLine) {
+            setSelectedOrderLine(null);
+        } else {
+            setSelectedOrderLine(orderLine);
+        }
+    }
 
-    const packData = [{ "orderLine": 3, "sku": "101-0911", "pickedQty": 1000, "remainingQty": 500, "description": "iPhone 12 Pro Max", "upc": "123456789014", }];
+    const packRow = () => {
+        setPackData(Array.of(pickData[selectedOrderLine], ...packData,));
+        pickData.splice(selectedOrderLine, 1);
+        setSelectedOrderLine(null);
+        setShowModal(false)
+    }
+    const unPackRow = () => {
+        setPickData(Array.of(packData[selectedOrderLine], ...pickData,));
+        packData.splice(selectedOrderLine, 1);
+        console.log(packData);
+        setSelectedOrderLine(null);
+    }
 
-    useEffect(() => { }, []);
+    const Action = () => {
+        const navigate = useNavigate();
+        const navigateToMenu = () => {
+            navigate('/');
+        };
+
+        const showModal = () => {
+            setShowModal(true);
+        }
+
+        return (
+            <div className="container text-center mb-3">
+                <button className="btn btn-primary mr-2" onClick={navigateToMenu}>Back</button>
+                <button className="btn btn-success mx-2" onClick={showModal}>Pack</button>
+                <button className="btn btn-warning mr-2" onClick={unPackRow}>Unpack</button>
+            </div>
+        );
+    }
+
+    const Modal = (prop) => {
+        return (
+            <div
+                className={`modal fade ${showModal ? 'show' : ''}`}
+                tabIndex="-1"
+                role="dialog"
+                style={{ display: showModal ? 'block' : 'none' }}
+            >
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Select number of <b>{prop.data !== undefined ? prop.data.sku : ""}</b> to pack</h5>
+                        </div>
+                        <div className="modal-body">
+                            <div className="input-group">
+                                <span className="input-group-text">Item Count</span>
+                                <input type="text" className="form-control" id="itemCount" placeholder={prop.data !== undefined ? prop.data.remainingQty : ""} />
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button
+                                type="button"
+                                className="btn btn-secondary"
+                                data-dismiss="modal"
+                                onClick={() => setShowModal(false)}
+                            >
+                                Close
+                            </button>
+                            <button type="button" className="btn btn-primary" onClick={packRow}>
+                                Pack
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 
     const TableHeader = () => {
         return (
@@ -27,19 +104,21 @@ export const Packing = () => {
         );
     };
 
-    //create a table body component using passed in data
     const TableBody = (data) => {
         const rows = data.prop.map((row, index) => {
+            // has issues with the selectedOrderLine
             return (
-                <tr key={index}>
+                <tr key={index} className={row.orderLine === selectedOrderLine ? 'table-active' : ''}
+                    onClick={() => toggleRow(row.orderLine)}>
                     <td>{row.orderLine}</td>
                     <td>{row.sku}</td>
                     <td>{row.pickedQty}</td>
                     <td>{row.remainingQty}</td>
                     <td>{row.description}</td>
                     <td>{row.upc}</td>
-                </tr>
+                </tr >
             );
+
         });
         return <tbody>{rows}</tbody>;
     };
@@ -48,22 +127,22 @@ export const Packing = () => {
         <Layout title="Packing" className="container table-responsive">
             <p className="lead">Please scan or enter Items to proceed</p>
             <div className="row">
-                <div className="col-4">
+                <div className="col-6">
                     <div className="card">
                         <div className="card-header">
-                            <b>Order</b> XXX
+                            <b>Order</b> {pickData[0].orderNumber}
                         </div>
                         <ul className="list-group list-group-flush">
-                            <li className="list-group-item"><b>Carton ID:</b> XXX</li>
+                            <li className="list-group-item"><b>Ticket ID:</b> {pickData[0].ticketNumber}</li>
                             <li className="list-group-item"><b>Customer:</b> XXX</li>
                             <li className="list-group-item"><b>Instructions:</b> XXX</li>
                         </ul>
                     </div>
                 </div>
-                <div className="col-4">
+                <div className="col-6">
                     <div className="input-group">
-                        <span className="input-group-text">Carton #</span>
-                        <input type="text" className="form-control" id="cartonNumber" />
+                        <span className="input-group-text">Ticket #</span>
+                        <input type="text" className="form-control" id="ticketNumber" />
                     </div>
                     <br />
                     <div className="input-group">
@@ -74,18 +153,19 @@ export const Packing = () => {
             </div>
             <br /><hr />
             <h3>PENDING</h3>
-            <table className="table table-bordered table-striped table-hover border-primary border-2">
+            <table className="table table-bordered table-hover border-primary">
                 <TableHeader />
                 <TableBody prop={pickData} />
             </table>
             <br />
             <h3>PACKED</h3>
-            <table className="table table-bordered table-striped table-hover border-primary border-2">
+            <table className="table table-bordered table-hover border-primary">
                 <TableHeader />
                 <TableBody prop={packData} />
             </table>
             <br />
             <Action />
+            <Modal data={pickData[selectedOrderLine - 1]} />
         </Layout>
     );
 };
